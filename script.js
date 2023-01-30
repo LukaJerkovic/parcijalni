e92da6152c9a0735169f51dc1f2772a0b5a2e17d
Pepui <lujerkovic@gmail.com>
2023-01-30 20:30:20 +0100

    Commit 2

--- script.js
+++ script.js
@@ -6,12 +6,17 @@ const list = document.querySelector('#list');
 const handleSearch = () => {
     const nameInput = input.value;
     console.log(nameInput);
+
+    if (nameInput.length < 2) {
+        return;
+    }
     
    const url = `https://itunes.apple.com/search?term=${nameInput}&entity=song`;
 
     const request = new XMLHttpRequest();
     request.open('GET', url, true);
     request.onload = () => {
+        list.innerHTML = '';
         if(request.status === 200 && nameInput.length > 2) {
             const responseObject = JSON.parse(request.response);
             const allResults = responseObject.results;
@@ -19,6 +24,7 @@ const handleSearch = () => {
         
         for (let i = 0; i < allResults.length; i++) {
             const songName = responseObject.results[i].trackName;
+            
 
             const newList = document.createElement(`li`);
             newList.innerText = songName;
